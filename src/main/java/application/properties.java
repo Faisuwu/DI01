9fada635eec7325d1a2159f1adf3b4ce06b98e2b
Faisuwu <105352928+Faisuwu@users.noreply.github.com>
2024-11-11 18:10:25 +0100

    Primer commit

---
+++ src/main/java/application/properties.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package application;
+
+/**
+ *
+ * @author faisu
+ */
+public class properties {
+    
+    /*
+    db.url=jdbc:sqlserver:
+    db.username=sa
+    db.password=Pwd1234.*/
+    
+}
---
+++ src/main/java/com/mycompany/di01/antonimaqueda/DI01AntoniMaqueda.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.di01.antonimaqueda;
+
+/**
+ *
+ * @author faisu
+ */
+public class DI01AntoniMaqueda {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
